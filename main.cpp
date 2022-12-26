3313c75f542d2e4a32cdbee74e12f24de55a23ec
Vianney Nguyen <nguyenvianney@gmail.com>
2022-12-25 17:45:15 -0700

    Add USB code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include "pico/stdlib.h"
+//#include "pico/stdlib.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "bsp/board.h"
+#include "tusb.h"
+
+#include "usb_descriptors.h"
 
 int main() {
+    board_init();
+    tusb_init();
+
+    while (true) {
+        tud_task();
+
+    }
 
     return 0;
 }
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+    if (report_type == HID_REPORT_TYPE_OUTPUT) {
+        if (report_id == REPORT_ID_KEYBOARD) {
+            if (bufsize < 1) return;
+
+            uint8_t const kbd_leds = buffer[0];
+            if (kbd_leds & KEYBOARD_LED_CAPSLOCK) {
+                board_led_write(true);
+            } else {
+                board_led_write(false);
+            }
+        }
+    }
+}
+
 
